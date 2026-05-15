9c3a90c605cfeb5d2d19629613dd1984141d5655
Ramba Ganesh <ammananna8513@gmail.com>
2026-05-15 22:07:20 +0530

    Created multiple routes in Next.js

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "40px",
+      }}
+    >
+      <h1>Welcome to Next.js Application</h1>
+      <p>This is Header Component</p>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <nav
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <h1>Navigation Bar</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
